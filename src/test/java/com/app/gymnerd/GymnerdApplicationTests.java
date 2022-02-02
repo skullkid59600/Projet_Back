519d04bee5818e94cd3b285e781212dbfc8ce9fd
Chiara <brichot.chiara@yahoo.fr>
2022-02-02 11:14:41 +0100

    :sparkles: : first commit

---
+++ src/test/java/com/app/gymnerd/GymnerdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.gymnerd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymnerdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
