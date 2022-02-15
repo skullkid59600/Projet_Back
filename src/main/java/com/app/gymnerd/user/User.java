a2a88ab08cb5818213b199ac65cc14d0852d410f
Chiara <brichot.chiara@yahoo.fr>
2022-02-15 17:00:06 +0100

    :sparkles: : add code for User

--- src/main/java/com/app/gymnerd/user/User.java
+++ src/main/java/com/app/gymnerd/user/User.java
@@ -1,4 +1,22 @@
 package com.app.gymnerd.user;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String login;
+    String password;
 }
--- src/main/java/com/app/gymnerd/user/UserRepository.java
+++ src/main/java/com/app/gymnerd/user/UserRepository.java
@@ -1,4 +1,7 @@
 package com.app.gymnerd.user;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByLogin(String login);
 }
