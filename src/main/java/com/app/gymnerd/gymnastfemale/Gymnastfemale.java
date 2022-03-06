028c4ed9af8f875f3dcff7e98c37f67d9f094eab
Chiara <brichot.chiara@yahoo.fr>
2022-03-06 06:12:05 +0100

    :sparkles: : constructors, getter & setter

--- src/main/java/com/app/gymnerd/gymnastfemale/Gymnastfemale.java
+++ src/main/java/com/app/gymnerd/gymnastfemale/Gymnastfemale.java
@@ -11,7 +11,7 @@ import javax.persistence.Id;
 import java.util.Date;
 
 @NoArgsConstructor
-@AllArgsConstructor
+//@AllArgsConstructor
 @Data
 @Entity
 public class Gymnastfemale {
@@ -30,4 +30,97 @@ public class Gymnastfemale {
     // height
     // active years
     // medals
+
+    public Gymnastfemale(Long id, String firstname, String lastname, String nickname, Boolean gender, String country, Date birthdate, String sociallinkone, String sociallinktwo, String img) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.nickname = nickname;
+        this.gender = gender;
+        this.country = country;
+        this.birthdate = birthdate;
+        this.sociallinkone = sociallinkone;
+        this.sociallinktwo = sociallinktwo;
+        this.img = img;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(Boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getSociallinkone() {
+        return sociallinkone;
+    }
+
+    public void setSociallinkone(String sociallinkone) {
+        this.sociallinkone = sociallinkone;
+    }
+
+    public String getSociallinktwo() {
+        return sociallinktwo;
+    }
+
+    public void setSociallinktwo(String sociallinktwo) {
+        this.sociallinktwo = sociallinktwo;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
 }
