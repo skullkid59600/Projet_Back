343ff77d18f205a27fdbbbe02fbf633afe69820d
Chiara <brichot.chiara@yahoo.fr>
2022-03-21 18:00:01 +0100

    :poop: : one to one relation

--- src/main/java/com/app/gymnerd/role/Role.java
+++ src/main/java/com/app/gymnerd/role/Role.java
@@ -1,38 +1,23 @@
 package com.app.gymnerd.role;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.app.gymnerd.user.User;
 
 import javax.persistence.*;
 
-@NoArgsConstructor
-@Data
 @Entity
 @Table(name = "ROLE")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+
+    @Column(name = "user_id")
+    Long userId;
+
+    @OneToOne
+    @PrimaryKeyJoinColumn(name = "user_id")
+    User user;
+
+    @Column(name = "name")
     String name;
 
-    public Role(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/com/app/gymnerd/user/User.java
+++ src/main/java/com/app/gymnerd/user/User.java
@@ -1,62 +1,24 @@
 package com.app.gymnerd.user;
 
 import com.app.gymnerd.role.Role;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
-@NoArgsConstructor
-@Data
 @Entity
 @Table(name = "USERS")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int id;
+
+    @Column(name = "id")
+    Long id;
+
+    @Column(name = "login")
     String login;
+
+    @Column(name = "password")
     String password;
-    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, fetch = FetchType.EAGER)
-    List<Role> roles = new ArrayList<Role>();
 
-    public User(int id, String login, String password, List<Role> roles) {
-        this.id = id;
-        this.login = login;
-        this.password = password;
-        this.roles = roles;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<Role> roles) {
-        this.roles = roles;
-    }
+    @OneToOne(mappedBy = "user")
+    Role role;
 }
