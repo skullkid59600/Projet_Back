f032478c633aa6674776617d5e7442a3e37b1707
Chiara <brichot.chiara@yahoo.fr>
2022-04-05 10:50:48 +0200

    :construction: : onetomany (test)

--- src/main/java/com/app/gymnerd/role/Role.java
+++ src/main/java/com/app/gymnerd/role/Role.java
@@ -15,7 +15,7 @@ public class Role {
     @Column(name = "user_id")
     Long userId;
 
-    @OneToOne
+    @OneToMany
     @PrimaryKeyJoinColumn(name = "user_id")
     User user;
 
--- target/classes/data.sql
+++ target/classes/data.sql
@@ -10,3 +10,4 @@
 insert into USERS(id, login, password) values (1, 'Chiara','test');
 insert into ROLE(name) values ('ROLE_ADMIN');
 insert into ROLE(name) values ('ROLE_USER');
+insert into ROLE(user_id) values (1);
